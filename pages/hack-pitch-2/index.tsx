2e9ff9c54d084171f16f25eafa1ac6d50ab6caea
Mouad-Bounfil <mouad.bounfil.it@gmail.com>
2024-12-14 10:00:40 +0100

    first projects

---
+++ pages/hack-pitch-2/index.tsx
@@ -0,0 +1,16 @@
+import { withLayout } from "@moxy/next-layout";
+import MainPlatformLayout from "@/layouts/main-platform-layout/main-platform-layout";
+import BanAuthWrapper from "@/layouts/ban-auth-wrapper/ban-auth-wrapper";
+import AppPage from "@/modules/community-project/templates/community";
+import HackPitch from "@/modules/community-project/templates/hack-pitch-2";
+import { Center } from "@mantine/core";
+
+function HomePage() {
+  return (
+    <Center>
+      <HackPitch />
+    </Center>
+  );
+}
+
+export default HomePage;
